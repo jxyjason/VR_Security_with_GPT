a92adfa2f2fd3ec06e848f7847c9911d88f9d080
jxyjason <jxyjason@163.com>
2023-12-13 08:14:53 +0800

    optimized the promopt

--- VR_Security_with_GPT.py
+++ VR_Security_with_GPT.py
@@ -4,9 +4,10 @@ import openai
 import xml.etree.ElementTree as ET
 import subprocess
 import shutil
+import time
 
-openai.api_key = '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'
-
+openai.api_key = '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'
+# openai.api_key = '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'
 def ask_gpt(messages):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -28,15 +29,10 @@ def decompilation(apk_name):
     # 返回输出结果和错误信息
     return output, error
 
-
-
-
-
-
-def analyzeOneApk(apk_name,description):
+def analyzeOneApk(apk_src,description):
     try:
         # 反编译apk，输出为app文件夹
-        decompilation(apk_name)
+        decompilation(apk_src)
         # 读取xml
         tree = ET.parse('app/AndroidManifest.xml')
         root = tree.getroot()
@@ -48,15 +44,16 @@ def analyzeOneApk(apk_name,description):
 
         # 初始化messages
         messages = [
-            {"role": "system", "content": "You are a helpful assistant."}
+            {"role": "system", "content": "You are an assistant with clear and concise assistant"}
         ]
 
-        # # 提问xml文件
-        # question = "Analyze which permissions and features are used by the decompiled xml file below (only need permissions and features):\n"+xml_string
-        # messages.append({"role": "user", "content": question})
-        # answer = ask_gpt(messages)
+        # 提问xml文件
+        question = "Analyze which permissions and features are used by the decompiled xml file below (only need permissions and features):\n"+xml_string
+        messages.append({"role": "user", "content": question})
+        answer = ask_gpt(messages)
+        xmlPermissionAndFeature = answer
         # print(answer)
-        # # 附加回答信息
+        # 附加回答信息
         # messages.append({"role": "assistant", "content": answer})
         # print("--------------------------------------------------\n")
 
@@ -69,13 +66,32 @@ def analyzeOneApk(apk_name,description):
         # messages.append({"role": "assistant", "content": answer})
         # print("--------------------------------------------------\n")
 
+        for i in range(21):
+            time.sleep(1)
+            print("sleep:" + str(i + 1) + "s")
+
+        # 重新开一个提问进行二阶提问，针对问题描述和已经提取好的xml文件
+        messages = [
+            {"role": "system", "content": "You are an assistant with clear and concise assistant"},
+            # {"role": "system", "content": "You are an assistant with clear and concise assistant"},#input
+            # {"role": "system", "content": "You are an assistant with clear and concise assistant"},
+        ]
+
         # 提问哪些不该用
-        question = "Based on the xml file and game description, what permissions and features should not be applied for according to the privacy policy:\ndescription:\n" + description +"\nxml file:\n" +xml_string
+        question = "Based on the Permissions And Features and game description,  which sensitive permissions and features should not be requested (Simply answer the permissions and features mentioned in the question). \n" \
+                   "** All you have to do is analyze what's below: **\n" \
+                    "\n```\n"+xmlPermissionAndFeature+"\n```\n" \
+                   "\ndescription:\n```\n" + description + "\n```\n" \
+                   # \
+                   # "\nAn Example:" \
+                   # "\nIf the Permissions And Features are : \n```\n android.permission.ACCESS_NETWORK_STATE\nandroid.permission.ACCESS_FINE_LOCATION\nandroid.permission.CAMERA\nandroid.permission.RECORD_AUDIO、\n ```"\
+                   # "\nAnd the description is :```在太空中，飞船突发意外，你需要几岁各式各样的陨石来通过不同挑战。需要和同伴一起攻克难关。```\n"\
+                   # "According to the description this game does not need to be online and does not require location information, so your answer is:\n"\
+                   # "```\nThe ACCESS_FINE_LOCATION and CAMERA is not required.\nBecause the description says that the game needs to be played with other players, then the RECORD_AUDIO is need.\n```"
         print(question)
         messages.append({"role": "user", "content": question})
         answer = ask_gpt(messages)
         print(answer)
-
 
         # 删除反编译文件夹
         shutil.rmtree("app")
@@ -87,7 +103,11 @@ def analyzeOneApk(apk_name,description):
             shutil.rmtree("app")
         print(f"Error occurred: {str(e)}")
 
+if __name__ == "__main__":
 
+    apk_src = "D:\\ict\\My_Project\\Security\\reverse\\reverse_app\\gpt_app\\9月的晚餐_SepsDiner_8592_19.apk"
+    description = "It is an online game. Sep’s Diner，这是一家由您担任主厨的新汉堡餐厅！它真的会成为镇上最好的汉堡吗？现在就看你了！一层又一层，您的体验将增加，美食客户的数量也会增加。他们很匆忙，所以在他们离开之前尽快满足他们！饥饿和不耐烦，他们不会让您犯错误…… 细心和精确，以获得最大的利润！包括全新的甜点餐厅Sep’s Donut！游戏特色：· 2间餐厅· 包括 3 种游戏模式：定时、轻松、多人· 每家餐厅包括 27 个级别（定时/休闲 12 个，多人游戏 3 个）· 最多 4 名玩家的多人合作游戏· 紧张刺激的关卡！· 身临其境的音频氛围· 不耐烦的客户用有趣的声音· 美丽的风景和彩灯· 逐级增加难度· 超过 30 种不同的汉堡食谱组合！· 煎饼、甜甜圈、华夫饼、纸杯蛋糕、冰淇淋和奶昔！"
+    analyzeOneApk(apk_src,description)
 
 
 
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import VR_Security_with_GPT as tool
 import pandas as pd
 import openpyxl
-
+import time
 
 
 # 读取单元格每行内容，根据第一列的文件名找到gpt_app中的apk，根据第二列的游戏描述，生成对应的非法权限作为回答并保存在第三列，读取的xml文件存在第四列
 # 单元格第一列内容应为对应apk名，且去掉.apk。注意名称要去掉空格。
+# 单元格第二列内容为对应apk的描述。
+
+
 if __name__ == "__main__":
 
     file_path = 'app_permission.xlsx'  # 替换为实际的文件路径
@@ -19,7 +22,7 @@ if __name__ == "__main__":
     workbook = openpyxl.load_workbook(file_path)
     sheet = workbook[sheet_name]
 
-    # 打印选定的数据。index是之前读取的时候的index，从1开始（实际第2行）。sheet.cell中索引就是实际的，因此需要+1
+    # 打印选定的数据。index是之前读取的时候的index，从0开始。但sheet.cell中索引是从1开始的。
     for index, row in selected_data.iterrows():
         try:
             print("\n\n#################################### "+str(index)+" ############################################################################################################################################################\n\n")
@@ -28,13 +31,21 @@ if __name__ == "__main__":
                 print("already wrote.")
                 continue
             # 提问
-            answer,xml_string = tool.analyzeOneApk("D:\\ict\\My_Project\\Security\\reverse\\reverse_app\\gpt_app\\"+row[0]+".apk",row[1])
+            apk_src = "D:\\ict\\My_Project\\Security\\reverse\\reverse_app\\gpt_app\\"+row[0]+".apk"
+            description = row[1]
+            answer,xml_string = tool.analyzeOneApk(apk_src,description)
             # 保存结果
             sheet.cell(row=index+1, column=3).value = answer
             sheet.cell(row=index+1, column=4).value = xml_string
             workbook.save(file_path)
+
+            for i in range(21):
+                time.sleep(1)
+                print("sleep:"+str(i+1)+"s")
+
         except Exception as e:
             print(f"Error occurred: {str(e)}")
+
 
 
 
